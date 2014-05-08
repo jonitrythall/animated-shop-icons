9158d125c4fc077594b0fed92bd69d0bb6bf315f
jonitrythall <jonithall@gmail.com>
2014-05-07 22:03:54 -0700

    adding all files

---
+++ demo.js
@@ -0,0 +1,22 @@
+// Get a list of all svg elements
+icons = document.querySelectorAll('.icon-hook');
+
+// Cycle through our list
+for (var i = 0; i < icons.length; i++) {
+  icons[i].addEventListener('click', function(event) {
+    event.preventDefault();
+ 
+    var icon = this;
+    var currentClass = icon.getAttribute('class'); // The starting class
+
+    console.log(icon);
+
+    if (currentClass.indexOf('active') > -1) { 
+      // Remove .active
+      icon.setAttribute('class', currentClass.replace(' active', ''));
+    } else { 
+      // Add .active
+      icon.setAttribute('class', currentClass + ' active');
+    }
+  }, false);
+}
